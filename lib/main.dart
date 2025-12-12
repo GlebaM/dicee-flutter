b4e1ec16aa91adbe6fbfc80122d82baaf992f297
GlebaM <michal.gleba.pl@gmail.com>
2025-12-12 09:28:47 +0100

    feat(MVP-1): working dicee app

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,64 @@ import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
-    MaterialApp(
+    MyDiceeApp(),
+  );
+}
+
+class MyDiceeApp extends StatefulWidget {
+  const MyDiceeApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MyDiceeApp> createState() => _MyDiceeAppState();
+}
+
+class _MyDiceeAppState extends State<MyDiceeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.red,
+      ),
       home: Scaffold(
-        backgroundColor: Colors.red,
+        body: DicePage(),
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.red[600],
+          elevation: 2,
+          shadowColor: const Color.fromARGB(180, 0, 0, 0),
         ),
-        body: DicePage(),
       ),
-    ),
-  );
+    );
+  }
 }
 
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Flex(
+        direction: Axis.horizontal,
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                print('Button 1 pressed!');
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                print('Button 2 pressed!');
+              },
+              child: Image.asset('images/dice2.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
