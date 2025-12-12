7decae3558ace31fe4462e443df930bf0c0df538
GlebaM <michal.gleba.pl@gmail.com>
2025-12-12 10:02:11 +0100

    feat(MVP-2): dicee fully working

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -35,30 +37,60 @@ class _MyDiceeAppState extends State<MyDiceeApp> {
   }
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  void rollDice() {
+    final random = Random();
+    setState(() {
+      leftDiceNumber = random.nextInt(6) + 1; // Generates 0–5, so add 1
+      rightDiceNumber = random.nextInt(6) + 1; // Generates 0–5, so add 1
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Flex(
         direction: Axis.horizontal,
         children: [
-          Expanded(
-            child: TextButton(
-              onPressed: () {
-                print('Button 1 pressed!');
-              },
-              child: Image.asset('images/dice1.png'),
-            ),
+          DiceButton(
+            diceNumber: leftDiceNumber,
+            onPressed: rollDice,
           ),
-          Expanded(
-            child: TextButton(
-              onPressed: () {
-                print('Button 2 pressed!');
-              },
-              child: Image.asset('images/dice2.png'),
-            ),
+          DiceButton(
+            diceNumber: rightDiceNumber,
+            onPressed: rollDice,
           ),
         ],
+      ),
+    );
+  }
+}
+
+class DiceButton extends StatelessWidget {
+  final int diceNumber;
+  final VoidCallback onPressed;
+
+  const DiceButton({
+    super.key,
+    required this.diceNumber,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TextButton(
+        onPressed: onPressed,
+        child: Image.asset('images/dice$diceNumber.png'),
       ),
     );
   }
